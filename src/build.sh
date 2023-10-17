8533138d4cbc6a4a5202cd5c6f3f2f1e08b8b717
Fernando Espíndola <fernando.espindola@adp.com>
2023-10-17 19:07:49 -0300

    Fixing build

--- src/build.sh
+++ src/build.sh
@@ -2,13 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-# python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-# TODO: Uncomments when add support to the database
-# python3.9 manage.py makemigrations --noinput
-# python3.9 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collect Static..."
-# TODO Uncomment when add suport to the database
-# python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
