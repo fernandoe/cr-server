8e9eb7152e7ac5ada8e4810b6f9f61fc63e89806
Fernando Espíndola <fer.esp@gmail.com>
2023-10-18 19:43:40 -0300

    Add suport to the yfinance (#4)

--- src/fe_cr/admin.py
+++ src/fe_cr/admin.py
@@ -3,7 +3,7 @@ import os
 
 from django.contrib import admin
 
-from .models import Ticker
+from .models import Ticker, TickerData
 
 # Register your models here.
 
@@ -12,3 +12,20 @@ from .models import Ticker
 class TickerMA(admin.ModelAdmin):
     search_fields = ("name", "is_enabled")
     list_display = ("get_uuid", "created_at", "updated_at", "name", "is_enabled")
+
+
+@admin.register(TickerData)
+class TickerDataMA(admin.ModelAdmin):
+    search_fields = ("ticker", "date", "open", "high", "low", "close", "volume")
+    list_display = (
+        "get_uuid",
+        "created_at",
+        "updated_at",
+        "ticker",
+        "date",
+        "open",
+        "high",
+        "low",
+        "close",
+        "volume",
+    )
