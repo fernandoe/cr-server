a4deaa77e3a145f3d7c9e3ca489161d8edbb25c0
Fernando Espíndola <fernando.espindola@adp.com>
2023-10-18 19:51:36 -0300

    Add pre-commit to the configuration and configured the code.

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ requirements.txt:
 
 vercel-dev:
 	node node_modules/vercel/dist/index.js dev
+
+pre-commit-all-files:
+	pre-commit run --all-files
--- src/fe_cr/apps.py
+++ src/fe_cr/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FeCrConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fe_cr'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fe_cr"
